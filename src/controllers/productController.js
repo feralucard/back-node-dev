e01b1c28a5c990e7503b3cfeec477c59246080c1
Fernando Mart <58201784+feralucard@users.noreply.github.com>
2023-10-23 12:07:46 -0600

    Update product images

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,22 @@
 const Product = require('../models/Product');
+const fs = require('fs');
+const path = require('path');
 
 exports.getProducts = async (req, res) => {
   try {
     const products = await Product.findAll();
+    
+    products.forEach(element => {
+      try {
+        // Leer la imagen del sistema de archivos
+        const imageBuffer = fs.readFileSync(path.join(__dirname, 'uploads', element.image));
+        const base64Image = imageBuffer.toString('base64');
+        element.image = base64Image;
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener la imagen' });
+      }
+    });
     res.json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -12,11 +26,20 @@ exports.getProducts = async (req, res) => {
 // Obtener un producto por su ID
 exports.getProductById = async (req, res) => {
     const productId = req.params.id;
-  
+   
     try {
       const product = await Product.findByPk(productId);
       if (!product) {
         return res.status(404).json({ message: 'Producto no encontrado' });
+      }
+      try {
+        // Leer la imagen del sistema de archivos
+        const imageBuffer = fs.readFileSync(path.join(__dirname, 'uploads', element.image));
+        const base64Image = imageBuffer.toString('base64');
+        element.image = base64Image;
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al obtener la imagen' });
       }
       res.json(product);
     } catch (error) {
@@ -25,9 +48,16 @@ exports.getProductById = async (req, res) => {
   };
 
 exports.createProduct = async (req, res) => {
-  const { name, price } = req.body;
+  let { name, price, image, marca } = req.body;
+  const buffer = Buffer.from(image, 'base64');
+  // Guardar la imagen en el sistema de archivos
+  const imageFilename = `${Date.now()}.png`;
+  fs.writeFileSync(path.join(__dirname, 'uploads', imageFilename), buffer);
+  // Guardar la ruta de la imagen en la base de datos
+  image = `${imageFilename}`;
+  console.log(image);
   try {
-    const product = await Product.create({ name, price });
+    const product = await Product.create({ name, price, image, marca });
     res.status(201).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
