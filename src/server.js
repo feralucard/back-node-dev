413a4e11dc5237ac451b6e9dc81e57a88559ce98
Fernando Mart <58201784+feralucard@users.noreply.github.com>
2023-10-23 12:32:16 -0600

    cors localhost

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const productOrderRoutes = require('./routes/productOrderRoutes');
@@ -6,16 +7,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Habilita CORS para cualquier origen
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // * significa que cualquier origen es permitido
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
-
-
+app.use(cors({ origin: 'http://localhost:4200' }));
 // Rutas para autenticación
 app.use('/api/auth', authRoutes);
+
 
 app.use('/api', productOrderRoutes);
 
